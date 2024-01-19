86ed841bcf4886beedaa0881119facba9e57512d
Jusman91 <jusmannine1@gmail.com>
2024-01-19 17:36:20 +0800

    added control-allow-origin

--- src/server.js
+++ src/server.js
@@ -16,18 +16,7 @@ import categoriesRouter from './routes/categories.js';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use((req, res, next) => {
-	res.header(
-		'Access-Control-Allow-Origin',
-		'http://localhost:5173',
-	);
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept',
-	);
-	res.header('Access-Control-Allow-Credentials', 'true');
-	next();
-});
+
 app.use(cors());
 app.use(morgan('dev'));
 app.use(cookieParser());
